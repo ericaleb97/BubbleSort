5b6dbbbb249a79b77da8273e053dafad58f1bd41
ericaleb97 <43590510+ericaleb97@users.noreply.github.com>
2021-05-21 16:01:02 -0700

    Add project files.

---
+++ BubbleSort/Algorithm.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BubbleSort
+{
+    public class Algorithm
+    {
+        public int[] unsortedArray { get; set; }
+
+        public void Inputs(int size)
+        {
+            Console.WriteLine("How big do you want the array to be?");
+            Console.WriteLine();
+
+            size = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.WriteLine("Enter your array:");
+
+            Console.WriteLine();
+
+            unsortedArray = new int[size];
+
+            for (int i = 0; i < unsortedArray.Length; i++)
+            {
+                unsortedArray[i] = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+
+        public void BubbleSort()
+        {
+            int unsortedArrayLength = unsortedArray.Length;
+
+            for (int i = 0; i < unsortedArrayLength - 1; i++)
+            {
+
+                for (int j = 0; j < unsortedArrayLength - i - 1; j++)
+                {
+                    if (unsortedArray[j] > unsortedArray[j + 1])
+                    {
+                        int temp = unsortedArray[j];
+                        unsortedArray[j] = unsortedArray[j + 1];
+                        unsortedArray[j + 1] = temp;
+                    }
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Sorted array:");
+
+            foreach (int num in unsortedArray)
+            {
+                Console.WriteLine(num);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ BubbleSort/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace BubbleSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = 0;
+            var algo = new Algorithm();
+            algo.Inputs(size);
+            algo.BubbleSort();
+        }
+    }
+}
